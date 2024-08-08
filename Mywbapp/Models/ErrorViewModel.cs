c816c5ae9963d5568cd958143c1c3115d10f4832
global <global@gmail.com>
2024-08-08 15:33:47 +0530

    Add project files.

---
+++ Mywbapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mywbapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
